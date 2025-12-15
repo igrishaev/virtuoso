ac390acd5bc84f4c87c0bfa9826a2cd706d2481b
Ivan Grishaev <ivan@grishaev.me>
2025-12-15 17:53:57 +0300

    update tests

--- test/virtuoso/v3_test.clj
+++ test/virtuoso/v3_test.clj
@@ -1,4 +1,6 @@
 (ns virtuoso.v3-test
+  (:import
+   (java.lang VirtualThread))
   (:require
    [clojure.test :refer [deftest is]]
    [virtuoso.v3 :as v]))
@@ -41,25 +43,95 @@
             (Thread/sleep 100)
             f))]
 
-    (is (= 52 @f))
+    (is (= 52 @f))))
 
 
+(deftest test-pvalues
+  (let [capture!
+        (atom #{})
 
-    )
+        result
+        (v/pvalues
+          (let [result (+ 1 1)]
+            (swap! capture! conj :a)
+            result)
+          (let [result (/ 0 0)]
+            (swap! capture! conj :b)
+            result)
+          (let [result (+ 2 2)]
+            (swap! capture! conj :c)
+            result))]
 
+    (is (= #{:a :c}
+           @capture!))
 
+    (is (= 2
+           (first result)))
 
+    (try
+      (nth result 1)
+      (is false)
+      (catch Exception e
+        (is true)))))
 
+(deftest test-thread
+  (let [t (v/thread
+            (Thread/sleep 1000)
+            (+ 1 2))]
+    (is (instance? VirtualThread t))
+    (is (.isAlive t))
+    (Thread/sleep 1000)
+    (is (not (.isAlive t)))))
 
-  )
+(deftest test-future
+  (let [f (v/future 1)]
+    (is (future? f))
+    (is (= 1 @f)))
+
+  (let [f (v/future
+            (/ 0 0))]
+    (is (future? f))
+    (try
+      @f
+      (is false)
+      (catch Exception e
+        (is e)))))
+
+(deftest test-map
+  (let [result (v/map inc [1 2 3])]
+    (is (= [2 3 4] result)))
+
+  (let [result (v/map / [4 6 3] [2 2 0])]
+    (is (= 2 (nth result 0)))
+    (is (= 3 (nth result 1)))
+    (try
+      (nth result 2)
+      (is false)
+      (catch Exception e
+        (is e))))
+
+  (let [result (v/map / [4 6 3] [2 2 3 0])]
+    (is (= [2 3 1] result))))
+
+(deftest test-pmap
+  (let [capture!
+        (atom #{})
+
+        result
+        (v/pmap 3
+                (fn [a b]
+                  (swap! capture! conj [a b])
+                  (/ a b))
+                [3 2 1 0]
+                [3 2 1 0])]
+
+    (is (= #{} @capture!))
+    (is (= 1 (first result)))
+    (is (= #{[2 2] [3 3] [1 1]} @capture!))))
+
 
 ;; with-executor
 ;; future-via
 ;; overrides
 ;; bindings?
-;; thread
-;; future
-;; map
 ;; fmap
-;; pmap capture evaluation
-;; pvalues
