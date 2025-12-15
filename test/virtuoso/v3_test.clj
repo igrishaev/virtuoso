ba8b48e5cae9f09ad73bfd3f5e4dd6be681a7a57
Ivan Grishaev <ivan@grishaev.me>
2025-12-15 12:40:12 +0300

    add tests

--- test/virtuoso/v3_test.clj
+++ test/virtuoso/v3_test.clj
@@ -3,6 +3,56 @@
    [clojure.test :refer [deftest is]]
    [virtuoso.v3 :as v]))
 
+(set! *warn-on-reflection* true)
+
+(def ^:dynamic *var* nil)
+
+(deftest test-future
+  (let [f (v/future (+ 1 2))]
+    (future? f)
+    (is (= 3 @f)))
+
+  (let [f (v/future (/ 1 0))]
+    (future? f)
+    (try
+      @f
+      (is false)
+      (catch Exception e
+        (is (instance? java.util.concurrent.ExecutionException
+                       e))
+        (is (= "java.lang.ArithmeticException: Divide by zero"
+               (ex-message e)))
+        (is (instance? ArithmeticException
+                       (ex-cause e))))))
+
+  (let [f
+        (v/future
+          (Thread/sleep 3000)
+          ::done)
+        v
+        (deref f 1000 ::timeout)]
+
+    (is (= v ::timeout)))
+
+  (let [f
+        (binding [*var* 42]
+          (let [f (v/future
+                    (+ *var* 10))]
+            (Thread/sleep 100)
+            f))]
+
+    (is (= 52 @f))
+
+
+
+    )
+
+
+
+
+
+  )
+
 ;; with-executor
 ;; future-via
 ;; overrides
