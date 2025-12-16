4ce1bc00292fdf4748db82aa7b2cc8953ef0eb87
Ivan Grishaev <ivan@grishaev.me>
2025-12-16 15:58:27 +0300

    update tests

--- test/virtuoso/v3_test.clj
+++ test/virtuoso/v3_test.clj
@@ -9,7 +9,7 @@
 
 (def ^:dynamic *var* nil)
 
-(deftest test-future
+(deftest test-future-1
   (let [f (v/future (+ 1 2))]
     (future? f)
     (is (= 3 @f)))
@@ -43,7 +43,15 @@
             (Thread/sleep 100)
             f))]
 
-    (is (= 52 @f))))
+    (is (= 52 @f)))
+
+  (with-redefs [double? (constantly ::abc)]
+    (let [f (binding [*var* 42]
+              (let [f (future
+                        (double? 42))]
+                (Thread/sleep 100)
+                f))]
+      (is (= ::abc @f)))))
 
 
 (deftest test-pvalues
@@ -83,7 +91,7 @@
     (.join t)
     (is (not (.isAlive t)))))
 
-(deftest test-future
+(deftest test-future-2
   (let [f (v/future 1)]
     (is (future? f))
     (is (= 1 @f)))
@@ -129,8 +137,64 @@
     (is (= 1 (first result)))
     (is (= #{[2 2] [3 3] [1 1]} @capture!))))
 
+(deftest test-)
+
+
 
 ;; with-executor
 ;; future-via
-;; overrides
-;; bindings?
+
+
+(defn sequential-pagination
+  [job-config
+   {:keys [request response next-request req-fn request-sleep-ms] :as params}
+   flow-data]
+  (iteration (fn [[prev-request prev-response]]
+               (when request-sleep-ms
+                 (System/gc)
+                 (Thread/sleep (min request-sleep-ms 15000)))
+               (when-some [req (conditionals/next-request-on-condition
+                                next-request
+                                job-config
+                                params
+                                (assoc flow-data
+                                       :prev-request  prev-request
+                                       :prev-response prev-response))]
+                 [req (req-fn req)]))
+             :initk [request response]))
+
+(->> data
+     (partition-all batch-size)
+     (process-batches! s3-client))
+
+(loop [data data]
+  (when-let [chunk (seq (take batch-size data))]
+    (process-batches! chunk)
+    (recur (drop batch-size data))))
+
+
+
+(defn get-request [job-config
+                   {:as params :keys [next-request
+                                      prev-request
+                                      prev-response]}
+                   flow-data]
+  (conditionals/next-request-on-condition
+   next-request
+   job-config
+   params
+   (assoc flow-data
+          :prev-request  prev-request
+          :prev-response prev-response)))
+
+
+(defn sequential-pagination
+  [job-config
+   {:as params :keys [req-fn]}
+   flow-data]
+  (loop [params params]
+    (when-let [request (get-request job-config params flow-data)]
+      (let [response (req-fn request)]
+        (recur (assoc params
+                      :prev-request request
+                      :prev-response response))))))
